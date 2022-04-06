8c6f93ae51293ef5e3a985f9b7af17087c793bd7
Leon Nguyen <leonnguyen1309@gmail.com>
2022-04-06 15:47:37 +0700

    feat, intergrate kai system - Remove deviceStatusPipe - Remove smart-table components - add new primeng package

--- src/app/@core/shared/pipes/device-status.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DeviceStatusPipe } from './device-status.pipe';
-
-describe('DeviceStatusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DeviceStatusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
