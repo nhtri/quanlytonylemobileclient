1f5afc832f5d13d24c54876fe2d01b30ac4dc532
tringuyen <tri.nh@hotmail.com>
2022-04-16 09:40:22 +0700

    up

--- src/app/pages/tables/thongke/thongke.component.ts
+++ src/app/pages/tables/thongke/thongke.component.ts
@@ -8,54 +8,232 @@ import * as XLSX from 'xlsx';
 })
 export class ThongkeComponent implements OnInit {
 
-  data = []
-  id = ""
+  datathu = []
+  datatempthu = []
+  datachi = []
+  datatempchi = []
+  datathuedit = []
+  datachiedit = []
+  datatong = []
+  datatongedit = []
+  datatongconvert = []
+  daterange = []
+
+
+
   date1 = ""
   date2 = ""
-  daterange = []
+  daterangefilter = []
   datatemp = []
-  totalmoney=0
+  totalthu=0
+  totalchi=0
   constructor(private service: NetworkserviceService) {
- 
-    this.service.getquanlythu().subscribe(val => {
+    // this.service.getquanlythu().subscribe(val => {
+    //   console.log(val)
+    //   this.datathu = val
+
+    // const res = this.datathu.reduce((acc, curr) => {
+    //   if (!acc[curr.ngaytao]) acc[curr.ngaytao] = []; //If this type wasn't previously stored
+    //   acc[curr.ngaytao].push(curr);
+    //   return acc;
+    // }, {});
+    // console.log('resthu', res)
+
+
+    // this.datathu.forEach(element => {
+    //   this.daterange.push(element.ngaytao)
+    //   if (res[element.ngaytao].length > 1) {
+    //     let tienchi = 0
+    //     let mucdich = ''
+    //     res[element.ngaytao].forEach(element => {
+    //       tienchi = tienchi + parseFloat(element.sotien)
+    //       mucdich = mucdich + ',' + element.mucdich
+    //     });
+    //     this.datathuedit.push({ 'ngaytao': element.ngaytao, 'tienthu': tienchi, 'mucdich': mucdich })
+
+    //   }
+    //   else {
+    //     this.datathuedit.push({ 'ngaytao': element.ngaytao, 'tienthu': element.sotien, 'mucdich': element.mucdich })
+    //   }
+    // });
+
+    this.service.getquanlythu().subscribe(async val => {
+      this.datathu = []
+      // this.sotien = "", this.date = "", this.mucdich = "", this.id = ""
       console.log(val)
       val.forEach(element => {
-        this.totalmoney += parseInt(element.sotien)
+        this.totalthu += parseInt(element.sotien)
         if (element.mucdich.includes('dh')) {
           this.service.getdanhsachdonhangquanlymobiletransaction([element.mucdich]).subscribe(data => {
             element.mucdich = 'Mã ĐH: ' + data[0].madonhang
-            this.data.push(element)
-            this.datatemp.push(element)
+            this.datathu.push(element)
           })
         }
         else {
-          this.data.push(element)
-          this.datatemp.push(element)
+          this.datathu.push(element)
         }
+
 
       });
 
+      await new Promise(f => setTimeout(f, 3000));
+      const res1 = this.datathu.reduce((acc, curr) => {
+        if (!acc[curr.ngaytao]) acc[curr.ngaytao] = []; //If this type wasn't previously stored
+        acc[curr.ngaytao].push(curr);
+        return acc;
+      }, {});
+      console.log('resthu', res1)
+
+      this.datathu.forEach(element => {
+        this.daterange.push(element.ngaytao)
+        if (res1[element.ngaytao].length > 1) {
+          let tienthu = 0
+          let mucdich = ''
+          res1[element.ngaytao].forEach(element => {
+            tienthu = tienthu + parseFloat(element.sotien)
+            mucdich = mucdich + ',' + element.mucdich
+          });
+          this.datathuedit.push({ 'ngaytao': element.ngaytao, 'tienthu': tienthu, 'mucdich': mucdich })
+
+        }
+        else {
+          this.datathuedit.push({ 'ngaytao': element.ngaytao, 'tienthu': element.sotien, 'mucdich': element.mucdich })
+        }
+      });
+      this.datathuedit = this.datathuedit.filter((value, index, self) =>
+        index === self.findIndex((t) => (
+          t.ngaytao === value.ngaytao
+        ))
+      )
+      console.log('datathuedit', this.datathuedit,)
+
+
+      console.log('datathu', this.datathu,)
+
+
+
+
+
+
+
+      this.service.getquanlychi().subscribe(val => {
+       
+        console.log(val)
+        this.datachi = val
+        this.datatempchi = val
+
+        const res = this.datachi.reduce((acc, curr) => {
+          if (!acc[curr.ngaytao]) acc[curr.ngaytao] = []; //If this type wasn't previously stored
+          acc[curr.ngaytao].push(curr);
+          return acc;
+        }, {});
+        console.log('res', res)
+
+        this.datachi.forEach(element => {
+          this.totalchi += parseInt(element.sotien)
+          this.daterange.push(element.ngaytao)
+          if (res[element.ngaytao].length > 1) {
+            let tienchi = 0
+            let mucdich = ''
+            res[element.ngaytao].forEach(element => {
+              tienchi = tienchi + parseFloat(element.sotien)
+              mucdich = mucdich + ',' + element.mucdich
+            });
+            this.datachiedit.push({ 'ngaytao': element.ngaytao, 'tienchi': tienchi, 'mucdich': mucdich })
+
+          }
+          else {
+            this.datachiedit.push({ 'ngaytao': element.ngaytao, 'tienchi': element.sotien, 'mucdich': element.mucdich })
+          }
+        });
+        this.datachiedit = this.datachiedit.filter((value, index, self) =>
+          index === self.findIndex((t) => (
+            t.ngaytao === value.ngaytao
+          ))
+        )
+        console.log('datachiedit', this.datachiedit,)
+
+
+        this.daterange = this.daterange.filter((value, index, self) =>
+          index === self.findIndex((t) => (
+            t === value
+          ))
+        )
+        console.log('this.daterange', this.daterange)
+
+
+
+
+        //data tong
+        this.datachiedit.forEach(element => {
+          if (this.daterange.includes(element.ngaytao)) {
+            this.datatong.push({ "ngaytao": element.ngaytao, "tienchi": element.tienchi, "mucdichchi": element.mucdich })
+          }
+        });
+        this.datathuedit.forEach(element => {
+          if (this.daterange.includes(element.ngaytao)) {
+            this.datatong.push({ "ngaytao": element.ngaytao, "tienthu": element.tienthu, "mucdichthu": element.mucdich })
+          }
+        });
+
+
+        let resdatatong
+        resdatatong = this.datatong.reduce((acc, curr) => {
+          if (!acc[curr.ngaytao]) acc[curr.ngaytao] = []; //If this type wasn't previously stored
+          acc[curr.ngaytao].push(curr);
+          return acc;
+        }, {});
+
+        console.log('this.datatong', this.datatong)
+        console.log('resdatatong', resdatatong)
+        this.datatong.forEach(element => {
+          if (resdatatong[element.ngaytao].length > 1) {
+            console.log("resdatatong[element.ngaytao]", resdatatong[element.ngaytao])
+            this.datatongedit.push({
+              "ngaytao": element.ngaytao, "tienthu": resdatatong[element.ngaytao][1].tienthu,
+              "mucdichthu": resdatatong[element.ngaytao][1].mucdichthu, "tienchi": resdatatong[element.ngaytao][0].tienchi, "mucdichchi": resdatatong[element.ngaytao][0].mucdichchi
+            })
+          }
+          {
+            if (element.mucdichthu != null && element.mucdichthu != "") {
+              this.datatongedit.push({
+                "ngaytao": element.ngaytao, "tienthu": element.tienthu,
+                "mucdichthu": element.mucdichthu, "tienchi": "", "mucdichchi": ""
+              })
+            }
+            else {
+              this.datatongedit.push({
+                "ngaytao": element.ngaytao, "tienthu": "",
+                "mucdichthu": "", "tienchi": element.tienchi, "mucdichchi": element.mucdichchi
+              })
+            }
+          }
+        });
+        this.datatongedit = this.datatongedit.filter((value, index, self) =>
+          index === self.findIndex((t) => (
+            t.ngaytao === value.ngaytao
+          ))
+        )
+        // this.datatongedit.forEach(element => {
+        //   this.service.getdanhsachdonhangquanlymobiletransaction([element.mucdichthu]).subscribe(data => {
+        //     element.mucdichthu = 'Mã ĐH: ' + data[0].madonhang
+
+        //   })
+        // });
+        this.datatemp = this.datatongedit
+        console.log('datatongedit', this.datatongedit)
+      });
 
 
     });
 
 
+
+
   }
-
-  fileName = "DanhSachThu"
-  date
-  sotien = ""
-  mucdich = ""
-  hinhthucthanhtoan = ""
-  
-
+  fileName = "DanhSachThuChi"
   ngOnInit(): void {
-    this.date = new Date().getFullYear() + '-' + (new Date().getMonth() + 1).toString().padStart(2, '0') + '-' + new Date().getDate().toString().padStart(2, '0')
-  }
 
-  selecthinhthucthanhtoan(event){
-    console.log(event.target.value)
-    this.hinhthucthanhtoan = event.target.value
   }
   exportexcel() {
     let element = document.getElementById('excel-table');
@@ -69,97 +247,26 @@ export class ThongkeComponent implements OnInit {
     XLSX.writeFile(wb, this.fileName);
   }
 
-  hoantat() {
-    console.log(this.sotien, this.mucdich, this.date)
-    if (this.id == "") {
-      this.service.quanlythu([this.sotien, this.date, this.mucdich, this.hinhthucthanhtoan]).subscribe(val => {
-        alert("Tạo mới thành công")
-        console.log(val)
-        this.service.getquanlythu().subscribe(val => {
-          console.log(val)
-          this.data = val
-        });
-      });
-    }
-    if (this.id != "") {
-      this.service.editquanlythu([this.sotien, this.date, this.mucdich, this.hinhthucthanhtoan, this.id]).subscribe(val => {
-        console.log(val)
-        alert("Chỉnh sửa thành công")
-        this.service.getquanlythu().subscribe(val => {
-          this.data = []
-          this.sotien = "", this.date = "", this.mucdich = "", this.id = ""
-          console.log(val)
-          val.forEach(element => {
-            if (element.mucdich.includes('dh')) {
-              this.service.getdanhsachdonhangquanlymobiletransaction([element.mucdich]).subscribe(data => {
-                element.mucdich = 'Mã ĐH: ' + data[0].madonhang
-                this.data.push(element)
-              })
-            }
-            else {
-              this.data.push(element)
-            }
 
-          });
-
-
-
-        });
-      });
-    }
-
-
-  }
-  edit(id, ngaytao, sotien, mucdich) {
-    this.date = ngaytao
-    this.sotien = sotien
-    this.mucdich = mucdich
-    this.id = id
-  }
-  delete(value) {
-    this.service.deletequanlythu([value]).subscribe(val => {
-      console.log(val)
-      alert("Xoá thành công")
-      this.service.getquanlythu().subscribe(val => {
-        this.data=[]
-        console.log(val)
-        val.forEach(element => {
-          if (element.mucdich.includes('dh')) {
-            this.service.getdanhsachdonhangquanlymobiletransaction([element.mucdich]).subscribe(data => {
-              element.mucdich = 'Mã ĐH: ' + data[0].madonhang
-              this.data.push(element)
-            })
-          }
-          else {
-            this.data.push(element)
-          }
-
-        });
-
-
-
-      });
-    });
-  }
 
   change1() {
     if (this.date2 == "") {
-      this.data = []
+      this.datatongedit = []
       console.log(this.date1, this.date2)
       this.datatemp.forEach(element => {
         console.log('element.ngaytao', element.ngaytao)
         if (this.date1 == element.ngaytao) {
-          this.data.push(element)
+          this.datatongedit.push(element)
         }
       });
     }
     if (this.date2 != "") {
-      this.data = []
-      this.daterange=[]
+      this.datatongedit = []
+      this.daterangefilter = []
       console.log(this.date1, this.date2)
       var currentDate = new Date(this.date1);
       while (currentDate <= new Date(this.date2)) {
-        this.daterange.push(currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0'));
+        this.daterangefilter.push(currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0'));
         currentDate.setDate(currentDate.getDate() + 1)
       }
 
@@ -167,33 +274,33 @@ export class ThongkeComponent implements OnInit {
         console.log('element.ngaytao', element1.ngaytao)
         this.daterange.forEach(element2 => {
           if (element2 == element1.ngaytao) {
-            this.data.push(element1)
+            this.datatongedit.push(element1)
           }
         });
 
       });
-      console.log(this.daterange)
+      console.log(this.daterangefilter)
     }
   }
   change2() {
-    this.data = []
-    this.daterange=[]
+    this.datatongedit = []
+    this.daterangefilter = []
     console.log(this.date1, this.date2)
     var currentDate = new Date(this.date1);
     while (currentDate <= new Date(this.date2)) {
-      this.daterange.push(currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0'));
+      this.daterangefilter.push(currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0'));
       currentDate.setDate(currentDate.getDate() + 1)
     }
 
     this.datatemp.forEach(element1 => {
       console.log('element.ngaytao', element1.ngaytao)
-      this.daterange.forEach(element2 => {
+      this.daterangefilter.forEach(element2 => {
         if (element2 == element1.ngaytao) {
-          this.data.push(element1)
+          this.datatongedit.push(element1)
         }
       });
 
     });
-    console.log(this.daterange)
+    console.log(this.daterangefilter)
   }
 }
