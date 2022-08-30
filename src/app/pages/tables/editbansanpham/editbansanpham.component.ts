9ed3be752d51284c1dea2824f3b76ef9937c8bff
Tri Nguyen <tri.nguyen1@tyme.com>
2022-08-30 10:13:54 +0700

    up

--- src/app/pages/tables/editbansanpham/editbansanpham.component.ts
+++ src/app/pages/tables/editbansanpham/editbansanpham.component.ts
@@ -587,7 +587,7 @@ export class EditbansanphamComponent implements OnInit {
                       }
                       // this.service.quanlythu([this.tienhoadon, date, transactionkey, this.hinhthucthanhtoan, this.vitri]).subscribe(val => { })
                       console.log('data danhsachdonhang', date, this.tongtienthu, transactionkey, soluongsanpham, danhsachimei.substring(0, danhsachimei.length - 1), this.vitri, this.hinhthucthanhtoan, this.tienhoadon)
-                      this.service.taodanhsachdonhang([date, this.tongtienthu, transactionkey, soluongsanpham, danhsachimei.substring(0, danhsachimei.length - 1), this.vitri, this.hinhthucthanhtoan, this.tienhoadon, this.tienmat, this.daikibi, this.chuyenkhoan, "luutam", this.thongtinkhachhang]).subscribe(value => {
+                      this.service.taodanhsachdonhang([date, this.tongtienthu, transactionkey, soluongsanpham, danhsachimei.substring(0, danhsachimei.length - 1), this.vitri, this.hinhthucthanhtoan, this.tongtienban, this.tienmat, this.daikibi, this.chuyenkhoan, "luutam", this.thongtinkhachhang]).subscribe(value => {
                         console.log(value)
                         alert("Lưu đơn hàng thành công")
                         if (this.vitri == "WAREHOUSE" && this.hinhthucthanhtoan == 'trahet') {
